b33c24aad794269b297d4f083f101c0f4d7bc3e4
Tom Leclercq <tomleclercq2006@gmail.com>
2025-02-16 11:49:21 +0100

    Ajout de la possibilité de spécifier le type de contrat lors de la recherche d'emploi

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ chrome_options = Options()
 chrome_service = Service("C:/chromedriver.exe")  # Mets ton chemin ici
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-# Demande de la query à l'utilisateur
 query = input("Entrez la query pour la recherche d'emploi (par exemple, 'python developer'): ")
 lieu = input("Entrez le lieu pour la recherche d'emploi (par exemple, 'Paris'): ")
 radius = input("Entrez le rayon de recherche (par exemple, '10km'): ")
-search_url = f"https://www.welcometothejungle.com/fr/jobs?query={query.replace(' ', '%20')}&refinementList%5Boffices.country_code%5D%5B%5D=FR&page=1&aroundLatLng=44.84044%2C-0.5805&aroundRadius={radius.replace(' ', '%20')}&aroundQuery={lieu.replace(' ', '%20')}"
+contract = input("Entrez le type de contrat (par exemple, 'internship=stage, apprenticeship=alternance, full_time=CDI'): ")
+search_url = f"https://www.welcometothejungle.com/fr/jobs?query={query.replace(' ', '%20')}&refinementList%5Boffices.country_code%5D%5B%5D=FR&page=1&aroundLatLng=44.84044%2C-0.5805&contract_type%5D%5B%5D={contract.replace(' ', '%20')}&aroundRadius={radius.replace(' ', '%20')}&aroundQuery={lieu.replace(' ', '%20')}"
 #search_url = f"https://www.welcometothejungle.com/fr/jobs?query={query.replace(' ', '%20')}"
 #search_url = "https://www.welcometothejungle.com/fr/jobs?query=python%20developer"
 driver.get(search_url)
