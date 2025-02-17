019f14e2b607a109a9d4c87cad0dcc0a2bf43846
Tom Leclercq <tomleclercq2006@gmail.com>
2025-02-17 09:55:14 +0100

    Ajout de la gestion des types de contrat et amélioration de l'affichage des annonces

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-
 # Configuration Selenium
 chrome_options = Options()
 # chrome_options.add_argument("--headless")  # Mode sans interface
@@ -15,7 +14,13 @@ driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 query = input("Entrez la query pour la recherche d'emploi (par exemple, 'python developer'): ")
 lieu = input("Entrez le lieu pour la recherche d'emploi (par exemple, 'Paris'): ")
 radius = input("Entrez le rayon de recherche (par exemple, '10km'): ")
-contract = input("Entrez le type de contrat (par exemple, 'internship=stage, apprenticeship=alternance, full_time=CDI'): ")
+contract = input("Entrez le type de contrat (par exemple, 'stage, alternance, cdi'): ")
+if contract == "stage":
+    contract = "internship"
+elif contract == "alternance":
+    contract = "apprenticeship"
+elif contract == "cdi":
+    contract = "full_time"
 search_url = f"https://www.welcometothejungle.com/fr/jobs?query={query.replace(' ', '%20')}&refinementList%5Boffices.country_code%5D%5B%5D=FR&page=1&aroundLatLng=44.84044%2C-0.5805&contract_type%5D%5B%5D={contract.replace(' ', '%20')}&aroundRadius={radius.replace(' ', '%20')}&aroundQuery={lieu.replace(' ', '%20')}"
 driver.get(search_url)
 
@@ -28,7 +33,6 @@ except Exception as e:
     print("Erreur lors du chargement de la page:", e)
     driver.quit()
 
-# Récupération de tous les éléments <li> dans la liste
 jobs_elements = job_list.find_elements(By.TAG_NAME, "li")
 print("li récupérés")
 
@@ -44,7 +48,6 @@ for job in jobs_elements:
         lien_element = job.find_element(By.CSS_SELECTOR, "a.sc-dnvZjJ.dhlcJw")
         lien = lien_element.get_attribute("href") if lien_element else "Lien non disponible"
 
-        # Affichage des résultats pour vérifier
         print(f"Titre: {title}, Entreprise: {entreprise}, Lieu: {lieu}, Lien: {lien}")
         
         jobs.append({"Titre": title,  "Entreprise": entreprise, "Lieu": lieu, "Lien": lien})
@@ -52,9 +55,16 @@ for job in jobs_elements:
         print("Erreur lors de l'extraction d'une offre:", e)
         continue
 
-driver.quit()
-
 # Sauvegarde des offres en CSV
 df = pd.DataFrame(jobs)
 df.to_csv("offres.csv", index=False, encoding='utf-8')
 print("Scraping terminé, offres enregistrées dans 'offres.csv' !")
+annonce = input("Quelle annonce voulez-vous voir ? (Entrez le numéro de l'annonce, annonce 1=0) ")
+if annonce == "":
+    driver.quit()
+print(jobs[int(annonce)])
+search_url = jobs[int(annonce)]["Lien"]
+driver.get(search_url)
+print("Page chargée avec succès.")
+input("Appuyez sur une touche pour quitter...")
+driver.quit()
